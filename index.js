85675f1abdb24764a33fd498a24e82c7919cd332
Roberto Busó-García <robertobuso@gmail.com>
2018-07-03 14:58:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
+  var nextNumber = 0;
+  
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name);
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
 }
 
 function nowServing(katzDeliLine) {
-  if (katzDeliLine.length === 0) {
-    return "There is nobody waiting to be served!";
-  } else {
-    return `Currently serving ${katzDeliLine.shift()}.`;
-  }
+  nextNumber++;
+    return `Currently serving number ${nextNumber}`;  
 }
 
 function currentLine (katzDeliLine) {
